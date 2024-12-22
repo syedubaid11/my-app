d1e6ed6dbd680ded7e9af18d89d66311aaa785d3
syedubaid <syedabuubaid@gmail.com>
2024-12-23 01:36:36 +0530

    commit

--- app/components/card.tsx
+++ app/components/card.tsx
@@ -1,7 +1,7 @@
 export const Card=()=>{
     return(
         <>
-        <div className="w-[200px] h-[200px] border-2 rounded-lg flex flex-col mt-[10px] mb-[10px]">
+        <div className="w-[200px] h-[200px] border-1 rounded-lg flex flex-col mt-[10px] mb-[10px] shadow-md">
             <div className="">
                 image
 
